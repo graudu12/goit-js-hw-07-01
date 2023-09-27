6a76ed9c1a9129b52b0d2528bd68f79fe65d1e01
Dmitrijs <graudu_12@yahoo.co.uk>
2023-09-27 10:54:25 +0100

    upd-hw-07-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,58 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('ul.gallery');
+let instance = null;
+
+const onCloseEsc = function (event) {
+  if (event.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', onCloseEsc);
+  }
+};
+
+const onGalleryClick = event => {
+  event.preventDefault();
+
+  const {
+    target: {
+      nodeName,
+      dataset: { source },
+    },
+  } = event;
+
+  if (nodeName !== 'IMG') {
+    return;
+  }
+
+  instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  document.addEventListener('keydown', onCloseEsc);
+};
+
+galleryList.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(galleryItem => {
+      const { preview, original, description } = galleryItem;
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
