89d7364541d32a9fae4ae1ec396c2ad80cbc6172
Dmitrijs <graudu_12@yahoo.co.uk>
2023-09-27 11:05:23 +0100

    upd-07-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('ul.gallery');
+
+const createCardsMarkup = cards => {
+  const { preview, original, description } = cards;
+  return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          title="${description}"
+        />
+      </a>
+  `;
+};
+
+const createCardsGallery = [...galleryItems].map(createCardsMarkup).join('');
+galleryList.insertAdjacentHTML('beforeend', createCardsGallery);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  fadeSpeed: 150,
+  animationSlide: false,
+});
